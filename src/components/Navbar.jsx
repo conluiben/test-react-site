ce082950b5a48f76da3e088b2a6980652d2fb320
CoreData Research <cdresearch@Luiz---CoreDatas-MacBook-Air.local>
2025-07-02 17:51:16 +0800

    fix: navbar and font
    - navbar becomes transparent onload and white on scroll
    - Calibri font locally imported

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,17 @@
 import iconHambuger from "../assets/icon-hamburger.svg";
+import cdLogoWhite from "../assets/cd-white.png";
+import { useEffect, useState } from "react";
 
 const Navbar = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      setScrolled(window.scrollY > 10); // threshold
+    };
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
   const menuItems = [
     "Home",
     "Who we are",
@@ -11,13 +22,25 @@ const Navbar = () => {
   ];
   return (
     <>
-      <div className="sticky top-0 flex items-stretch gap-x-0 h-[80px] bg-white z-10">
+      <div
+        className={`fixed w-full top-0 flex items-stretch gap-x-0 h-[80px] z-10 duration-200 ${
+          scrolled ? "bg-white" : "bg-transparent"
+        }`}
+      >
         <div className="flex items-center px-4 lg:hidden">
           <img src={iconHambuger} alt="Menu Icon" className="w-[20px]" />
         </div>
-        <div className="grow flex items-center px-8 gap-x-16">
+        <div
+          className={`grow flex items-center px-8 gap-x-16 ${
+            scrolled ? "bg-white" : "bg-transparent"
+          }`}
+        >
           <img
-            src="https://assets.coredataresearch.com/coredata.com.au/images/core-data-logo-retina.png"
+            src={
+              scrolled
+                ? "https://assets.coredataresearch.com/coredata.com.au/images/core-data-logo-retina.png"
+                : cdLogoWhite
+            }
             alt="CoreData logo"
             className="max-h-[40px]"
           />
